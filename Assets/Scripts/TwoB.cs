d09caa5d3b72c519732626f15e74f25b9f3b9a7e
MeeSh0o <ljhwuwu@qq.com>
2020-08-15 10:07:48 +0800

    初始版本

---
+++ Assets/Scripts/TwoB.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoB : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+/*
+ * 功能需求写在这里
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
