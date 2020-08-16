c3bb67a550b584dd08e9b946b071bfb0b0cfc739
Aaronczww <785333692@qq.com>
2020-08-16 15:57:04 +0800

    场景1、2

---
+++ Assets/Scripts/LastBack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LastBack : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    public void LastBackGround()
+    {
+        SceneManager.LoadScene("GameOver");
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
