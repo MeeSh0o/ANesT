9a1bcfe97a60ca72dbf1e091edb7abb41cc62afb
MeeSh0o <ljhwuwu@qq.com>
2020-08-16 12:43:54 +0800

    临时提交

---
+++ Assets/Scripts/BackGround.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGround : MonoBehaviour
+{
+    public GameObject[] picture = new GameObject[4];
+    public Vector3[] cameraPos = new Vector3[4];
+    public int currentScene;
+
+    private void Awake()
+    {
+        
+    }
+
+    public void SetCurrentScene(int scene)
+    {
+
+    }
+}
